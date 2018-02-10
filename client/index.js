f5bcd59ac7b8aa39bcb21a1570242aedbeba43fb
Ivan Storck <ivan@ivanstorck.com>
2018-02-09 18:59:07 -0800

    serverless WIP

--- client/index.js
+++ client/index.js
@@ -2,35 +2,43 @@
 // using Aerobatic CORS/HTTP Proxy endpoint set up in aerobatic.yml
 // data format documentation: http://www.ndbc.noaa.gov/measdes.shtml
 // split 3rd line by space
-const cacheBuster = new Date().toISOString()
-fetch('https://seattletide.aerobatic.io/wpow1?' + cacheBuster, {
-  mode: 'cors'
-}).then(function(res) {
-  return res.text()
-}).then(function(conditions) {
-  //process the lines of the text file
-  const conditionsLines = conditions.split('\n')
-  const currentConditions = conditionsLines[2]
-  const fixedCurrentConditions = currentConditions.replace(/  */g," ")
-  //process the columns of the line of text
-  const currentConditionsArray = fixedCurrentConditions.split(' ')
-  window.res = currentConditionsArray
-  const direction = currentConditionsArray[5]
-  // speed and gust are provided in meters per second
-  // multiply by 2.236936 to get miles per hour (or round up)
-  const convertMetersPerSecondToMilesPerHour = 2.24
-  const speed = currentConditionsArray[6] * convertMetersPerSecondToMilesPerHour
-  const gust = currentConditionsArray[7] * convertMetersPerSecondToMilesPerHour
-  const barometer = currentConditionsArray[12]
-  // temp is provided in degrees Celcius
-  const convertCtoF = function convert(c) { return ( c * (9/5) ) + 32 }
-  const temp = convertCtoF(currentConditionsArray[13])
-  const round = function round(x) {
-    return Math.round(x * 100) / 100
-  }
-  // update the html elements
-  document.getElementById('direction').innerText = direction
-  document.getElementById('speed').innerText = round(speed)
-  document.getElementById('gust').innerText = round(gust)
-  document.getElementById('temp').innerText = round(temp)
+const domain = "https://seattletide.aerobaticapp.com";
+const endpoint = "wpow1?";
+const cacheBuster = new Date().toISOString();
+fetch(domain + endpoint + cacheBuster, {
+  mode: "cors"
 })
+  .then(function(res) {
+    return res.text();
+  })
+  .then(function(conditions) {
+    //process the lines of the text file
+    const conditionsLines = conditions.split("\n");
+    const currentConditions = conditionsLines[2];
+    const fixedCurrentConditions = currentConditions.replace(/  */g, " ");
+    //process the columns of the line of text
+    const currentConditionsArray = fixedCurrentConditions.split(" ");
+    window.res = currentConditionsArray;
+    const direction = currentConditionsArray[5];
+    // speed and gust are provided in meters per second
+    // multiply by 2.236936 to get miles per hour (or round up)
+    const convertMetersPerSecondToMilesPerHour = 2.24;
+    const speed =
+      currentConditionsArray[6] * convertMetersPerSecondToMilesPerHour;
+    const gust =
+      currentConditionsArray[7] * convertMetersPerSecondToMilesPerHour;
+    const barometer = currentConditionsArray[12];
+    // temp is provided in degrees Celcius
+    const convertCtoF = function convert(c) {
+      return c * (9 / 5) + 32;
+    };
+    const temp = convertCtoF(currentConditionsArray[13]);
+    const round = function round(x) {
+      return Math.round(x * 100) / 100;
+    };
+    // update the html elements
+    document.getElementById("direction").innerText = direction;
+    document.getElementById("speed").innerText = round(speed);
+    document.getElementById("gust").innerText = round(gust);
+    document.getElementById("temp").innerText = round(temp);
+  });
---
+++ index.js
@@ -0,0 +1,20 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const requestp = require("request-promise");
+const app = express();
+const domain = "http://www.ndbc.noaa.gov/data/realtime2/WPOW1.txt";
+
+// const requestWeatherInfo = function() {
+// return; // a promise
+// };
+
+app.get("/wpow1", function(req, res) {
+  // requestWeatherInfo.then(function() {
+  //   res.send("foo");
+  // });
+  requestp(domain).then(function(text) {
+    res.send(text);
+  });
+});
+
+module.exports.handler = serverless(app);
