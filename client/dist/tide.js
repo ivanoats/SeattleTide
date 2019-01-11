db40b04d5c934788355158e5459432d071c0b638
Ivan Storck <ivan@ivanstorck.com>
2019-01-10 21:22:51 -0800

    tide info

--- client/dist/tide.js
+++ client/dist/tide.js
@@ -9,12 +9,17 @@ fetch(currentTideUri, {
   .then(function(jsontxt) {
     const tide = JSON.parse(jsontxt)
     const currentTide = tide.data[tide.data.length - 1]
-    console.log(currentTide)
+    document.getElementById('current-tide').innerText = currentTide.v
   })
 
 const predictionsUri =
   // 'https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=westpointwinddotcom&date=latest&datum=MLLW&station=9447130&time_zone=lst_ldt&units=english&interval=hilo&format=json'
   'https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=westpointwinddotcom&begin_date=20180910&end_date=20180911&datum=MLLW&station=9447130&time_zone=lst_ldt&units=english&interval=hilo&format=json'
+
+const NWSDateToJSDate = nwsdate => {
+  const jsdate = new Date(nwsdate)
+  return `${jsdate.toLocaleDateString()} ${jsdate.toLocaleTimeString()}`
+}
 
 fetch(predictionsUri, {
   mode: 'cors'
@@ -24,5 +29,15 @@ fetch(predictionsUri, {
   })
   .then(function(jsontxt) {
     const predictions = JSON.parse(jsontxt)
-    console.log(predictions)
+    window.predictions = predictions.predictions
+    const nextTide = `${NWSDateToJSDate(predictions.predictions[0].t)} ${
+      predictions.predictions[0].v
+    } ft ${predictions.predictions[0].type}`
+    const nextTideAfter = `${NWSDateToJSDate(predictions.predictions[1].t)} ${
+      predictions.predictions[1].v
+    } ft ${predictions.predictions[1].type}`
+    document.getElementById('next-tide').innerHTML = `<ul>
+   <li>${nextTide} </li>
+   <li>${nextTideAfter} </li>
+    </ul>`
   })
